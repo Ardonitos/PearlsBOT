917a1af5d12163331d77e3384444356913e1edca
Rafael Silveira <144189737+DevR-Silveira@users.noreply.github.com>
2024-06-08 21:52:15 -0300

    Contagem Update and Fix on Sync
    - Adicionado um comando de contagem global;
    - criado um comando de sync para evitar ultrapassar o limite da API do discord;
    - pequena revisão de código para torna-lo mais legivel

--- perolasbot.py
+++ perolasbot.py
@@ -7,21 +7,32 @@ from discord.ext.commands import has_permissions, MissingPermissions
 from openai import OpenAI
 from db_manager import Database
 
-BOT_KEY, OPENAI_KEY = getenv('BOT_KEY'), getenv('OPENAI_API_KEY')
+BOT_KEY, OPENAI_KEY, OWNER_ID = getenv('BOT_KEY'), getenv('OPENAI_API_KEY'), getenv('OWNER_ID')
 
 db = Database('pearls.db')
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
 
 client = OpenAI(api_key=OPENAI_KEY)
-bot = commands.Bot(command_prefix='a!', intents=intents)
+bot = commands.Bot(command_prefix='a!', intents=intents, activity=discord.Game('/ajuda na sua cara'), owner_id=OWNER_ID)
 
 @bot.event
 async def on_ready():
     print(f'Logado como {bot.user.name}.')
+
+@bot.event
+async def on_command_error(ctx:commands.Context, error):
+    print(error)
+    print(error.__traceback__)
+    await ctx.reply('Amigo, acho que esse comando não existe.')
+
+
+@bot.command()
+@commands.is_owner()
+async def sync(ctx:commands.Context):
+    await ctx.send('Initiating Sync...')
     await bot.tree.sync()
-    await bot.change_presence(activity=discord.Game('/ajuda na sua cara.'))
+    await ctx.send('Sync Sucessfully.')
 
 
 @bot.tree.command(name='ajuda', description='mostra o menu de ajuda, e seus comandos')
@@ -38,6 +49,7 @@ async def slash_command(interactions:discord.Interaction):
     embed.add_field(name='a!verperolas <página>', value='Use para ver todas as pérolas presentes no servidor.', inline=False)
     embed.add_field(name='a!perolas', value="Use para ver uma pérola aleatória.", inline=False)
     embed.add_field(name='a!c', value='Use para conversar com o próprio bot.')
+    embed.add_field(name='a!contagem', value='Que tal contribuir para nossa contagem?', inline=False)
     embed.add_field(name='Duvidas ou Sugestões', value='Incomode o ardonitos, ele vai te atender melhor que eu.', inline=False)
 
     await interactions.response.send_message(embed=embed)
@@ -125,38 +137,38 @@ async def perolas(ctx):
 @bot.command()
 async def verperolas(ctx: commands.Context, page='1'):
     server = ctx.guild
-    page_ = int(page)
+    actual_page = int(page)
 
     def loader(first, last):
         loader_ = db.read_data(table_name=f'sv{server.id}', otherstmt=f'WHERE id BETWEEN {first} AND {last}')
         return loader_
     
     def embed_content(loader):
-        counter = 0
+        pearls_per_page_counter = 0
         for i in loader:
             embed.add_field(name=f'Pérola #{i[0]}', value=f'"{i[1]}"', inline=False)
-            counter = i[0]
-        embed.set_footer(text=f'Página {page_}/{total_pages} - Vendo {counter}-{total_pearl} Pérolas.')
+            pearls_per_page_counter = i[0]
+        embed.set_footer(text=f'Página {actual_page}/{total_pages} - Vendo {pearls_per_page_counter}-{total_of_pearls} Pérolas.')
         
 
     try:
         embed = discord.Embed(color=discord.Colour.pink(), title=f'Vendo as pérolas de {server.name}.')
         embed.set_thumbnail(url=server.icon.url)
 
-        total_pearl: int = db.read_data('id', f'sv{server.id}')[-1][0]
-        pages_var = total_pearl/8
-        total_pages = pages_var if pages_var.is_integer() else floor(pages_var)+1 
+        total_of_pearls: int = db.read_data('id', f'sv{server.id}')[-1][0]
+        number_of_pages = total_of_pearls/8
+        total_pages = number_of_pages if number_of_pages.is_integer() else floor(number_of_pages)+1 
 
-        if page_ < 1 or page_ > total_pages:
+        if actual_page < 1 or actual_page > total_pages:
             raise IndexError
         else:
-            first_var = 8*(page_-1)+1
-            last_var = 8*page_
+            first_var = 8*(actual_page-1)+1
+            last_var = 8*actual_page
 
-            if page_ == 1:
+            if actual_page == 1:
                 embed_content(loader(1, 8))
                 await ctx.send(embed=embed)
-            elif page_ == total_pages:
+            elif actual_page == total_pages:
                 embed_content(loader(first_var, last_var))
                 await ctx.send(embed=embed)
             else:
@@ -167,8 +179,25 @@ async def verperolas(ctx: commands.Context, page='1'):
     except IndexError as ie:
         print(ie)
         await ctx.reply('Calma lá, eu ainda não anotei tanta pérola assim.')
-    except Exception as e:
-        print(e)
-        return
+    except Exception as error:
+        print(error)
+    
+
+@bot.command()
+async def contagem(ctx:commands.Context):
+    try:
+        db.create_table('contagem', 'numbers INTEGER')
+        if len(db.read_data('*', 'contagem')) == 0:
+            db.insert_data('contagem', '1')
+            await ctx.send(f'{ctx.author} comecou a contagem em 1.')
+            return
+
+        rows = db.read_data('*', 'contagem')
+        actual_number = rows[-1][0] +1
+        db.update_data('contagem', f'numbers={actual_number}', f'numbers={actual_number-1}')
+        await ctx.send(f'{ctx.author} continuou em {actual_number}.')
+    except Exception as error:
+        print(error)
+
 
 bot.run(BOT_KEY)
